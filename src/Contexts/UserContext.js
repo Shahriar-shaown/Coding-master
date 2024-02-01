def6ac96fff537e74c2b84e6377bf464a3edc38f
Shahriar-shaown <shaownryn@gmail.com>
2024-02-01 06:04:05 +0600

    firebase added

--- src/Contexts/UserContext.js
+++ src/Contexts/UserContext.js
@@ -1,11 +1,40 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import app from '../firebase/firebase.config';
+import {GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut} from 'firebase/auth'
 export const AuthContext = createContext()
 
+const auth = getAuth(app)
+const googleProvider = new GoogleAuthProvider()
 const UserContext = ({children}) => {
     const [user, setUser] = useState([])
     
+    const createUser = (email,password)=>{
+        return createUserWithEmailAndPassword(auth,email,password)
+    }
+
     
-    const authInfo = {}
+
+    useEffect( () =>{
+        const unsubscribe = onAuthStateChanged(auth, currentUser=>{
+                setUser(currentUser)
+            })
+            return ()=>unsubscribe();
+    },[])
+
+    const logInUser = (email, password) =>{
+        return signInWithEmailAndPassword(auth,email,password)
+    }
+
+    const googleSignIn = () =>{
+        return signInWithPopup(auth,googleProvider)
+    }
+
+    const logOut = () =>{
+        return signOut(auth)
+    }
+
+    const authInfo = {user,createUser,logInUser,googleSignIn,logOut}
+
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
